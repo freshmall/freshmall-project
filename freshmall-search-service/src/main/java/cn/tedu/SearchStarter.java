8d1e605b37b80186f038b7f54ce8852f39ea731f
pt <756469140@qq.com>
2019-11-21 18:06:25 +0800

    全文检索

--- freshmall-search-service/src/main/java/cn/tedu/SearchStarter.java
+++ freshmall-search-service/src/main/java/cn/tedu/SearchStarter.java
@@ -1,5 +1,6 @@
 package cn.tedu;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
