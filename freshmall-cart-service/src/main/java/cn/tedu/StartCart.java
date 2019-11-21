e4932f3beaaab7809ce75a562de352ff476af6f4
pt <756469140@qq.com>
2019-11-21 19:22:09 +0800

    更新

--- freshmall-cart-service/src/main/java/cn/tedu/StartCart.java
+++ freshmall-cart-service/src/main/java/cn/tedu/StartCart.java
@@ -3,7 +3,6 @@ package cn.tedu;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
@@ -14,7 +13,9 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 public class StartCart {
     public static void main(String[] args) {
+
         SpringApplication.run(StartCart.class, args);
+
     }
 
     @Bean
