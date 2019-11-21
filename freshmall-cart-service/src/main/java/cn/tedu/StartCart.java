1a3da424ec6cd9263c1eee6c5c1af6b1fcb20d46
root <root>
2019-11-21 14:17:57 +0800

    提pom

--- freshmall-cart-service/src/main/java/cn/tedu/StartCart.java
+++ freshmall-cart-service/src/main/java/cn/tedu/StartCart.java
@@ -3,6 +3,7 @@ package cn.tedu;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
